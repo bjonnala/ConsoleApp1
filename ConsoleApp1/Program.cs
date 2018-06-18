969a05632ec953e726ea8373e9c5802fe0b8b1d3
HOTB\rjonnala <rjonnala@counselordirect.com>
2018-06-18 15:08:24 -0700

    Added unit tests

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,30 +10,42 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Configuring Dependency Injection
+            /*The first thing we do is configure the dependency injection container by creating a ServiceCollection, 
+             * adding our dependencies, and finally building an IServiceProvider*/
             var services = new ServiceCollection();
             services.AddSingleton<IData,Data>();
             services.AddSingleton<INetwork, Network>();
 
+            /*The serviceProvider is our container we can use to resolve services in our application. */
             var serviceProvider = services.BuildServiceProvider();
+            
+            
             // Making Client Calls
-            var bar = serviceProvider.GetService<IData>();
+            var client = serviceProvider.GetService<IData>();
+
+
+            // Create
+            RequestJSON createrequestJSON = new RequestJSON
+            {
+                user = "PQR34534, Inc"
+            };
+            Console.WriteLine(client.CreateUpdateAsync(createrequestJSON).Result); // parameter id is an optional field for CreateUpdateAsync method 
+            //and will be passed in only for during update'
 
             // Read
-            //Console.WriteLine(bar.ReadAsync("4E4458434948444647514D4D4B485944").Result);
+            Console.WriteLine(client.ReadAsync("4D4C4B484D4C4C564254574C50485949").Result);
 
-            //// Create
-            //RequestJSON createrequestJSON = new RequestJSON
-            //{
-            //    user = "XYZ, Inc"
-            //};
-            //Console.WriteLine(bar.CreateUpdateAsync(requestJSON).Result);
+            //Update
+            RequestJSON updaterequestJSON = new RequestJSON
+            {
+                expire = "1427822745",
+                user = "Cylance, Inc"
+            };
+            Console.WriteLine(client.CreateUpdateAsync(updaterequestJSON, "4C47514B4357535642485644544E5945").Result);
 
-            //// Update
-            //RequestJSON updaterequestJSON = new RequestJSON
-            //{
-            //    user = "XYZ, Inc"
-            //};
-            //Console.WriteLine(bar.CreateUpdateAsync(requestJSON).Result);
+
+            // Delete
+            client.DeleteAsync("4C47514B4357535642485644544E5945");
 
             Console.ReadKey();
         }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,15 +1,63 @@
 using ConsoleApp1;
+using ConsoleApp1.DAL;
+using ConsoleApp1.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private readonly Network _network;
+        private readonly Data _data;
+
+        public UnitTest1()
         {
-            Network u = new Network();
+            _network = new Network();
+            _data = new Data(_network);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            RequestJSON createrequestJSON = new RequestJSON
+            {
+                user = "MAC, Inc"
+            };
+
+            string actualres = _data.CreateUpdateAsync(createrequestJSON).Result;
+            Assert.IsTrue(actualres.Contains("Guid sucessfully created"));
+        }
+
+        [TestMethod]
+        public void TestRead()
+        {
+            string actualres = _data.ReadAsync("4C47514B4357535642485644544E5945").Result;
+            string expectedres = "Guid sucessfully created 4C47514B4357535642485644544E5945 and expires in 1427822745";
+            Assert.AreEqual(actualres, expectedres);
+
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+            RequestJSON updaterequestJSON = new RequestJSON
+            {
+                expire = "1427822745",
+                user = "Cylance, Inc"
+            };
+
+            string actualres = _data.CreateUpdateAsync(updaterequestJSON, "4C47514B4357535642485644544E5945").Result;
+            Assert.IsTrue(actualres.Contains("Guid sucessfully created"));
+
+        }
+
+        [TestMethod]
+        public async Task TestDelete()
+        {
+            await _data.DeleteAsync("4C47514B4357535642485644544E5945");
         }
     }
 }
